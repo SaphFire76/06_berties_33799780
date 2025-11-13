ca59d5027ef6ec6fd20adb245523d447320feb31
Abrar <araha002@gold.ac.uk>
2025-11-13 21:12:46 +0000

    Lab 6 Completion

--- routes/books.js
+++ routes/books.js
@@ -6,9 +6,20 @@ router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
 
-router.get('/search-result', function (req, res, next) {
+router.get('/search_result', function (req, res, next) {
+    let keyword = req.query.search_text;
     //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?"; // query database to get all the books
+    
+    let searchData = [`%${keyword}%`];
+
+    // execute sql query
+    db.query(sqlquery, searchData, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("list.ejs", {availableBooks:result})
+    });
 });
 
 router.get('/list', function(req, res, next) {
@@ -19,7 +30,37 @@ router.get('/list', function(req, res, next) {
         if (err) {
             next(err)
         }
-        res.send(result)
+        res.render("list.ejs", {availableBooks:result})
+    });
+});
+
+router.get('/addbook', function(req, res, next) {
+    res.render('addbook.ejs')
+});
+
+router.post('/bookadded', function (req, res, next) {
+    // saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    // execute sql query
+    let newrecord = [req.body.name, req.body.price]
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        else
+            res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price)
+    })
+})
+
+router.get('/bargainbooks', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books WHERE price<20"; // query database to get all the books
+    
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("list.ejs", {availableBooks:result})
     });
 });
 
