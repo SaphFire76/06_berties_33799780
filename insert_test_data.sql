722d04203d9019407f5e06ddb9a80aa3223f20cf
Abrar <araha002@gold.ac.uk>
2025-11-20 23:39:06 +0000

    Password Hash Updated

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,4 +3,4 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
-INSERT INTO userDetails (username, fname, lname, email, hashedPass) VALUES ('gold','Gold','Smiths','goldsmiths@example.com','$2a$10$neoNDXUUzZ1fRyokENek5.6o3TX1PnJ29eF8xDeEtoy0fnRMVilJK') ;
+INSERT INTO userDetails (username, fname, lname, email, hashedPass) VALUES ('gold','Gold','Smiths','goldsmiths@example.com','$2a$10$qNi1mjRsTZaEM8QRymAOJuEvHq9Kc8PhyqxLd9ut8RjewGdRh0T4i') ;
